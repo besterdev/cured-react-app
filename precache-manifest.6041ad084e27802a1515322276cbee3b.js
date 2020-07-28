5078243bf2a12f92893d8eb983cab0bd84cfde8d
thawatchai <thawatchai.krai@gmail.com>
2020-07-28 12:02:17 +0700

    Updates

---
+++ precache-manifest.6041ad084e27802a1515322276cbee3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d40b8827c826ca46a427e3566bb0d13",
+    "url": "/cured-react-app/index.html"
+  },
+  {
+    "revision": "a5d6323d375e971974df",
+    "url": "/cured-react-app/static/css/main.1071dc8d.chunk.css"
+  },
+  {
+    "revision": "ddfcf7362dace9eeb398",
+    "url": "/cured-react-app/static/js/2.31d85363.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cured-react-app/static/js/2.31d85363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d6323d375e971974df",
+    "url": "/cured-react-app/static/js/main.ee95f42d.chunk.js"
+  },
+  {
+    "revision": "3f93e9d787b11d337c07",
+    "url": "/cured-react-app/static/js/runtime-main.5f68a557.js"
+  }
+]);
